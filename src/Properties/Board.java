4bcb55bcb59a992acaad081fbfa3eb5da5e0cfe8
ChrisBetancur <chrisbetancur321@gmail.com>
2021-02-26 15:38:18 -0500

    Test Update

--- src/Properties/Board.java
+++ src/Properties/Board.java
@@ -1,13 +1,28 @@
 package Properties;
 
+/**
+ * Board --- Creates the board which contains all properties related to the board
+ * @author Chris Betancur
+ * @author David Alvarez Tamayo
+ */
+
 public class Board {
 
-    private Tile[][] board = new Tile[64][64];
+    private Tile[][] board;
 
     public Board() {
+        board = new Tile[64][64];
     }
 
     public Board(Tile[][] board) {
+        this.board=board;
+    }
+
+    public Tile[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(Tile[][] board) {
         this.board = board;
     }
 }
--- src/Properties/Snake.java
+++ src/Properties/Snake.java
@@ -2,8 +2,33 @@ package Properties;
 
 import java.util.ArrayList;
 
+/**
+ * Snake --- Create Snake which contains all properties relating to Snake
+ */
+
 public class Snake {
 
-    private ArrayList<Tile> currentTiles = new ArrayList<>();
-    
+    private int snakeSize;
+    private ArrayList<Tile> currentTiles;
+
+    public Snake(int snakeSize) {
+        this.snakeSize = snakeSize;
+        this.currentTiles = new ArrayList<>();
+    }
+
+    public int getSnakeSize() {
+        return snakeSize;
+    }
+
+    public void setSnakeSize(int snakeSize) {
+        this.snakeSize = snakeSize;
+    }
+
+    public ArrayList<Tile> getCurrentTiles() {
+        return currentTiles;
+    }
+
+    public void setCurrentTiles(ArrayList<Tile> currentTiles) {
+        this.currentTiles = currentTiles;
+    }
 }
