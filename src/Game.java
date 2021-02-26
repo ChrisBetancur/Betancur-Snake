cb81689ac618331f5f7f69ed7d83fc5712cc79e1
ChrisBetancur <chrisbetancur321@gmail.com>
2021-02-26 16:35:47 -0500

    Multi-Dimensional Arrays

--- src/Game.java
+++ src/Game.java
@@ -3,5 +3,44 @@
 public class Game {
     public static void main(String[] args){
 
+        int[][][] arr = new int[3][][];
+
+        int[] arr1 = new int[3];
+
+        /**
+         * {1,4,3,2,4,2,4,5} 1-D array
+         *
+         *
+         *
+         * arr[0]-{41,34,3}
+         * arr[1]-{56,3,1}
+         * arr[2]-{34,68,6}
+         */
+
+        /*for(int i = 0;i<arr1.length;i++){
+            arr1[i]=10;
+        }
+
+        for(int i = 0;i<arr1.length;i++){
+            System.out.print(arr1[i]+",");
+        }
+
+        for(int row = 0;row<arr.length;row++){
+            arr[row] = new int[5];
+            for(int col=0;col< arr[row].length;col++){
+                arr[row][col]=5;
+            }
+        }
+        System.out.println();
+        System.out.println();
+        for(int i = 0;i<arr.length;i++){
+            System.out.print("Row "+i+": ");
+            for(int j = 0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+", ");
+            }
+            System.out.println();
+        }*/
+
+
     }
 }
--- src/Properties/Board.java
+++ src/Properties/Board.java
@@ -1,5 +1,7 @@
 package Properties;
 
+import java.util.ArrayList;
+
 /**
  * Board --- Creates the board which contains all properties related to the board
  * @author Chris Betancur
@@ -9,13 +11,24 @@ package Properties;
 public class Board {
 
     private Tile[][] board;
+    private Snake snake;
+    private final int DEFAULT_SNAKE_SIZE=5;
 
     public Board() {
         board = new Tile[64][64];
+        snake = new Snake(DEFAULT_SNAKE_SIZE);
     }
 
     public Board(Tile[][] board) {
         this.board=board;
+    }
+
+    private void initBoard(){
+
+    }
+
+    private void initSnake(){
+
     }
 
     public Tile[][] getBoard() {
